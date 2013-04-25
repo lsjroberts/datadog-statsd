87fee10c2156ebee2527ed602c7faca81ccdc673
Laurence Roberts <lsjroberts@gmail.com>
2013-04-25 16:33:12 +0100

    Updated to PSR-2 coding standards

--- src/DataDog/Statsd.php
+++ src/DataDog/Statsd.php
@@ -13,13 +13,14 @@ namespace DataDog;
  * @author Alex Corley <anthroprose@gmail.com>
  **/
  
-class Statsd {
+class Statsd
+{
 
-	static private $__server = 'localhost';
-    static private $__datadogHost;
-    static private $__eventUrl = '/api/v1/events';
-    static private $__apiKey;
-    static private $__applicationKey;
+    private static $server = 'localhost';
+    private static $datadogHost;
+    private static $eventUrl = '/api/v1/events';
+    private static $apiKey;
+    private static $applicationKey;
 
     /**
      * Log timing information
@@ -28,10 +29,9 @@ class Statsd {
      * @param float $time The ellapsed time (ms) to log
      * @param float|1 $sampleRate the rate (0-1) for sampling.
      **/
-    public static function timing($stat, $time, $sampleRate = 1, array $tags = null) {
-    	
-        static::send(array($stat => "$time|ms"), $sampleRate, $tags);
-		
+    public static function timing($stat, $time, $sampleRate = 1, array $tags = null)
+    {
+        static::send(array($stat => "$time|ms"), $sampleRate, $tags);   
     }
 
     /**
@@ -41,10 +41,9 @@ class Statsd {
      * @param float $value The value
      * @param float|1 $sampleRate the rate (0-1) for sampling.
      **/
-    public static function gauge($stat, $value, $sampleRate = 1, array $tags = null) {
-
+    public static function gauge($stat, $value, $sampleRate = 1, array $tags = null)
+    {
         static::send(array($stat => "$value|g"), $sampleRate, $tags);
-
     }
 
     /**
@@ -54,10 +53,9 @@ class Statsd {
      * @param float $value The value
      * @param float|1 $sampleRate the rate (0-1) for sampling.
      **/
-    public static function histogram($stat, $value, $sampleRate = 1, array $tags = null) {
-
+    public static function histogram($stat, $value, $sampleRate = 1, array $tags = null)
+    {
         static::send(array($stat => "$value|h"), $sampleRate, $tags);
-
     }
 
     /**
@@ -67,10 +65,9 @@ class Statsd {
      * @param float $value The value
      * @param float|1 $sampleRate the rate (0-1) for sampling.
      **/
-    public static function set($stat, $value, $sampleRate = 1, array $tags = null) {
-
+    public static function set($stat, $value, $sampleRate = 1, array $tags = null)
+    {
         static::send(array($stat => "$value|s"), $sampleRate, $tags);
-
     }
 
 
@@ -81,10 +78,9 @@ class Statsd {
      * @param float|1 $sampleRate the rate (0-1) for sampling.
      * @return boolean
      **/
-    public static function increment($stats, $sampleRate = 1, array $tags = null) {
-        	
+    public static function increment($stats, $sampleRate = 1, array $tags = null)
+    {
         static::updateStats($stats, 1, $sampleRate, $tags);
-		
     }
 
     /**
@@ -94,10 +90,9 @@ class Statsd {
      * @param float|1 $sampleRate the rate (0-1) for sampling.
      * @return boolean
      **/
-    public static function decrement($stats, $sampleRate = 1, array $tags = null) {
-    	
+    public static function decrement($stats, $sampleRate = 1, array $tags = null)
+    {
         static::updateStats($stats, -1, $sampleRate, $tags);
-		
     }
 
     /**
@@ -106,95 +101,81 @@ class Statsd {
      * @param string|array $stats The metric(s) to update. Should be either a string or array of metrics.
      * @param int|1 $delta The amount to increment/decrement each metric by.
      * @param float|1 $sampleRate the rate (0-1) for sampling.
-	 * @param array|string $tags Key Value array of Tag => Value, or single tag as string
-	 * 
+     * @param array|string $tags Key Value array of Tag => Value, or single tag as string
+     * 
      * @return boolean
      **/
-    public static function updateStats($stats, $delta = 1, $sampleRate = 1, array $tags = null) {
-    	
-        if (!is_array($stats)) { $stats = array($stats); }
-		
+    public static function updateStats($stats, $delta = 1, $sampleRate = 1, array $tags = null)
+    {
+        if (!is_array($stats)) {
+            $stats = array($stats);
+        }
+        
         $data = array();
-		
+        
         foreach($stats as $stat) {
-        	
             $data[$stat] = "$delta|c";
-			
         }
 
         static::send($data, $sampleRate, $tags);
-		
     }
 
     /**
      * Squirt the metrics over UDP
      * @param array $data Incoming Data
      * @param float|1 $sampleRate the rate (0-1) for sampling.
-	 * @param array|string $tags Key Value array of Tag => Value, or single tag as string
-	 * 
+     * @param array|string $tags Key Value array of Tag => Value, or single tag as string
+     * 
      * @return null
      **/
-    public static function send($data, $sampleRate = 1, array $tags = null) {
-
+    public static function send($data, $sampleRate = 1, array $tags = null)
+    {
         // sampling
         $sampledData = array();
 
-		if ($sampleRate < 1) {
-        	
+        if ($sampleRate < 1) {
             foreach ($data as $stat => $value) {
-            	
                 if ((mt_rand() / mt_getrandmax()) <= $sampleRate) {
-                	
                     $sampledData[$stat] = "$value|@$sampleRate";
-					
-				}
-				
-			}
-			
-		} else {
-        	
+                }
+            }
+        } else {
             $sampledData = $data;
-			
-		}
+        }
 
-        if (empty($sampledData)) { return; }
-	
-		// Non - Blocking UDP I/O - Use IP Addresses!
-		$socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-		socket_set_nonblock($socket);
-		
+        if (empty($sampledData)) {
+            return;
+        }
+    
+        // Non - Blocking UDP I/O - Use IP Addresses!
+        $socket = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+        socket_set_nonblock($socket);
+        
         foreach ($sampledData as $stat => $value) {
+            if ($tags !== NULL && is_array($tags) && count($tags) > 0) {
+                $value .= '|';
 
-			if ($tags !== NULL && is_array($tags) && count($tags) > 0) {
-				
-				$value .= '|';
-				
-				foreach ($tags as $tag_key => $tag_val) {
-					
-					$value .= '#' . $tag_key . ':' . $tag_val . ',';
-					
-				}
-				
-				$value = substr($value, 0, -1);
-				
-			} elseif (isset($tags) && !empty($tags)) {
-				
-				$value .= '|#' . $tags;
-				
-			}
-        	
-			socket_sendto($socket, "$stat:$value", strlen("$stat:$value"), 0, static::$__server, 8125);
-			
-		}
+                foreach ($tags as $tag_key => $tag_val) {
+                    $value .= '#' . $tag_key . ':' . $tag_val . ',';
+                }
 
-		socket_close($socket);
-		
+                $value = substr($value, 0, -1);
+            } elseif (isset($tags) && !empty($tags)) {
+                $value .= '|#' . $tags;
+            }
+
+            socket_sendto($socket, "$stat:$value", strlen("$stat:$value"), 0, static::$server, 8125);
+        }
+
+        socket_close($socket);
+        
     }
 
-    public static function configure($apiKey, $applicationKey, $datadogHost = 'https://app.datadoghq.com') {
-        static::$__apiKey = $apiKey;
-        static::$__applicationKey = $applicationKey;
-        static::$__datadogHost = $datadogHost;
+    public static function configure($apiKey, $applicationKey, $datadogHost = 'https://app.datadoghq.com')
+    {
+        static::$apiKey = $apiKey;
+        static::$applicationKey = $applicationKey;
+        static::$datadogHost = $datadogHost;
     }
 
     /**
@@ -207,7 +188,8 @@ class Statsd {
      *   http://api.datadoghq.com/events for the valid keys
      * @return null
      **/
-    public static function event($title, $vals = array()) {
+    public static function event($title, $vals = array())
+    {
         // Assemble the request
         $vals['title'] = $title;
         // Convert a comma-separated string of tags into an array
@@ -222,9 +204,9 @@ class Statsd {
         $body = json_encode($vals); // Added in PHP 5.3.0
 
         // Get the url to POST to
-        $url = static::$__datadogHost . static::$__eventUrl
-             . '?api_key='            . static::$__apiKey
-             . '&application_key='    . static::$__applicationKey;
+        $url = static::$datadogHost . static::$eventUrl
+             . '?api_key='          . static::$apiKey
+             . '&application_key='  . static::$applicationKey;
 
         // Set up the http request. Need the PECL pecl_http extension
         $r = new HttpRequest($url, HttpRequest::METH_POST);
@@ -238,7 +220,4 @@ class Statsd {
             error_log($ex);
         }
     }
-
 }
-
-?>
